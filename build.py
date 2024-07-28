5cb082c31b80a88af6d0214d8cf635372e14df67
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2024-07-28 17:10:07 +0200

    Update build.py for building extension
    Adopt version extraction from parsing the ast to manifest.
    
    Signed-off-by: Ralph Sennhauser <ralph.sennhauser@gmail.com>

--- build.py
+++ build.py
@@ -2,26 +2,24 @@
 # This file is part of 0 A.D.
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-import ast, os, zipfile
+import os
+import tomllib
+import zipfile
+
 
 def get_version():
-    with open("io_scene_pyrogenesis/__init__.py", "r", encoding='UTF-8') as f:
-        data = f.read()
+    with open("io_scene_pyrogenesis/blender_manifest.toml", "rb") as f:
+        manifest = tomllib.load(f)
+        return manifest["version"]
 
-    ast_data = ast.parse(data)
-    for node in ast_data.body:
-        if node.__class__ == ast.Assign:
-            if len(node.targets) == 1:
-                if getattr(node.targets[0], "id", "") == "bl_info":
-                    break
-    version = ast.literal_eval(node.value)["version"]
-    return str(version[0]) + '.' + str(version[1]) + '.' + str(version[2])
 
 def build_archive():
     os.makedirs("dist", exist_ok=True)
     with zipfile.ZipFile(os.path.join("dist", "io_scene_pyrogenesis-" + get_version() + ".zip"), mode="w") as archive:
         archive.write("io_scene_pyrogenesis/__init__.py")
+        archive.write("io_scene_pyrogenesis/blender_manifest.toml")
         archive.write("LICENSE", arcname="io_scene_pyrogenesis/LICENSE")
+
 
 if __name__ == '__main__':
     build_archive()
